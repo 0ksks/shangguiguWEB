80998786dbb9cb6bf48709bb75f67fc08f21c7f6
yxluo <1625436933@qq.com>
2024-05-04 12:25:41 +0800

    add prettier formatter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import App from '@/App.vue'
 import ElementPlus from 'element-plus'
 import GlobalComponent from '@/components'
-import axios from 'axios'
 
 import 'element-plus/dist/index.css'
 import 'element-plus/dist/locale/zh-cn.mjs'
@@ -11,19 +10,10 @@ import '@/styles/index.scss'
 import { createApp } from 'vue'
 import { zhCn } from 'element-plus/es/locales.mjs'
 
-axios({
-    url: '/api/user/login',
-    method: 'post',
-    data: {
-        username: 'admin',
-        password: '1111110',
-    }
-});
-
-const app = createApp(App);
+const app = createApp(App)
 
 app.use(ElementPlus, {
-    locale: zhCn
-});
-app.use(GlobalComponent);
-app.mount('#app');
+  locale: zhCn,
+})
+app.use(GlobalComponent)
+app.mount('#app')
