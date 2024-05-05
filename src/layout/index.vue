1ac78be5f6dd02540cee39e21bf992ea64c24219
yxluo <1625436933@qq.com>
2024-05-05 17:27:02 +0800

    formatted

---
+++ src/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="LayoutContainer">
+    <h1>layout</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+  .LayoutContainer {
+    width: 100%;
+    height: 100vh;
+    background: red;
+  }
+</style>
